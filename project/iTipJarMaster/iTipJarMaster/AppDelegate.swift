548b65eca02c1ca9878e579a4497b614fc5d535e
Steven Rosenberg <MinnesotaSteve@users.noreply.github.com>
2014-10-08 21:41:54 -0500

    IAP code added
    SKProductsRequestDelegate in ViewController
    SKPaymentTransactionObserver in AppDelegate

--- project/iTipJarMaster/iTipJarMaster/AppDelegate.swift
+++ project/iTipJarMaster/iTipJarMaster/AppDelegate.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import StoreKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-                            
-  var window: UIWindow?
+class AppDelegate: UIResponder, UIApplicationDelegate, SKPaymentTransactionObserver {
+    
+    var window: UIWindow?
+    
+    
+    func paymentQueue(queue: SKPaymentQueue!, updatedTransactions transactions: [AnyObject]!) {
+        
+        for transaction in transactions as [SKPaymentTransaction]{
+            println(transaction.payment.productIdentifier)
+            
+            switch(transaction.transactionState){
+                
+            case SKPaymentTransactionState.Purchased:
+                println("Purchased")
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+                
+            case SKPaymentTransactionState.Purchasing :
+                println("Purchasing")
+                
+            case SKPaymentTransactionState.Restored :
+                println("restored")
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+                
+            case SKPaymentTransactionState.Deferred :
+                println("deferred")
+                
+            case SKPaymentTransactionState.Failed :
+                println("Failed: \(transaction.error)")
+                
+            }
+        }
+    }
+    
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        var defaultQueue: SKPaymentQueue  = SKPaymentQueue ()
+        defaultQueue.addTransactionObserver(self)
+        
+        return true
+    }
 
-
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
-  }
 
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
